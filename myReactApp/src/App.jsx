55328cb8099ce0b60259101629043ccd8548439a
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-04-11 09:01:35 -0500

    added notes on fragments in the main App(), these allow multiple html renders, we could use a div, but fragments don't create extra dom elements, if we want to do that we can use <div></div> instead

--- myReactApp/src/App.jsx
+++ myReactApp/src/App.jsx
@@ -3,11 +3,16 @@
 
 
 export default function App(){
-  return <form className="add-item-form">
+  return (
+  <>
+  <form className="add-item-form">
     <div className="row-form">
       <label htmlFor="item">My Items</label>
       <input type="text" id="item" />
     </div>
     <button className="btn">Add Item</button>
   </form>
+  <h1 className="h1">My To-Do List</h1>
+  </>
+  )
 }
